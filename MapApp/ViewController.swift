28feee277cec1592fac4cbe9e58e0f300e6a44df
iammikeyoung <myoung10@gmail.com>
2018-05-15 14:28:02 -0400

    Basic UI and map view implemented.

--- MapApp/ViewController.swift
+++ MapApp/ViewController.swift
@@ -7,19 +7,33 @@
 //
 
 import UIKit
+import MapKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var mapView: MKMapView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func standardPressed(_ sender: UIBarButtonItem) {
+        mapView.mapType = MKMapType.standard
     }
 
-
+    @IBAction func satellitePressed(_ sender: UIBarButtonItem) {
+        mapView.mapType = MKMapType.satellite
+    }
+    
+    @IBAction func hybridPressed(_ sender: UIBarButtonItem) {
+        mapView.mapType = MKMapType.hybrid
+    }
+    
+    @IBAction func locateMePressed(_ sender: UIBarButtonItem) {
+    }
+    
+    @IBAction func directionsPressed(_ sender: UIBarButtonItem) {
+    }
 }
 
